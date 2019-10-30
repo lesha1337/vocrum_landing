41d97cee3333453d216aa186c0f777e5c977771a
lesha1337 <lesha.32-2@yandex.ru>
2019-10-31 02:00:13 +0300

    build

---
+++ Frontend/build/precache-manifest.0a8eef0a5f040304da68a33f8205608b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee48549396d12ca0aa118bc5cd32f2d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd4fb74697ac7ae30690",
+    "url": "/static/css/main.1fc54493.chunk.css"
+  },
+  {
+    "revision": "101fc4e56422ccaa96a8",
+    "url": "/static/js/2.120cf881.chunk.js"
+  },
+  {
+    "revision": "fd4fb74697ac7ae30690",
+    "url": "/static/js/main.fb0d886a.chunk.js"
+  },
+  {
+    "revision": "2e4bc1b3c022e2e6153b",
+    "url": "/static/js/runtime-main.d54b910c.js"
+  }
+]);
